f194ace674f0ed16e94dad8aa643f9d0a27f19c8
Michael Chew <michael.chew@actifio.com>
2019-11-22 10:13:51 +1100

    Updated scripts

--- scpsetup.sh
+++ scpsetup.sh
@@ -4,13 +4,13 @@ oracle_host=syd-ora12-2
 
 for ff in cleanall.sh cleandb.sh cleanhr.sql cleanup.sql mountdb.sh unmountdb.sh queryhr.sql verify_db.sql
 do
-	scp $ff root@$oracle_host:$ff /home/oracle
+	scp $ff root@$oracle_host:/home/oracle
 	ssh root@$oracle_host "chown oracle:oinstall /home/oracle/$ff"
 	ssh root@$oracle_host "chmod 755 /home/oracle/$ff"
 done
 
 for ff in pre.sh post.sh
 do
-	scp $ff root@$oracle_host:$ff /act/scripts  
+	scp $ff root@$oracle_host:/act/scripts  
 	ssh root@$oracle_host "chmod 755 /home/oracle/$ff"
 done
