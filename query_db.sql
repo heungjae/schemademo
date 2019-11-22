91d978cbc57fa531a482643e81af3708ac080743
Michael Chew <michael.chew@actifio.com>
2019-11-22 15:25:17 +1100

    Updated scripts

--- query_db.sql
+++ query_db.sql
@@ -8,12 +8,14 @@ col status format a15
 col file_name format a95
 col host_name format a25
 col startuptime head "STARTUP TIME" format a25
+col scn_tstamp head "SCN TIMESTAMP" format a45
 col instance_number head INST# format 99
 col owner format a25
 col cnt head "# TABLES" format 99999
 select distinct tablespace_name, file_name from dba_data_files order by 1;
 select owner,count(*) cnt from dba_tables where owner in ('SCOTTY', 'HR') group by owner order by 1;
 select dbid, name, open_mode from v$database;
+select SCN_to_timestamp(current_scn) scn_tstamp, current_scn from V$database;
 select sum(bytes)/(1024*1024*1024) "DB size (GB)" from dba_data_files;
 select instance_number, instance_name, host_name, version, TO_CHAR(startup_time,'DD-MON-YYYY HH24:MI:SS') startuptime, status, logins, database_status from v$instance;
 exit
